9733f95155e17bb71ea6fc9760cbc669fe9b599b
Val_pcd <pacaud.valerian@gmail.com>
2024-05-06 13:53:30 +0200

    zaz

--- html/Page_connexion.php
+++ html/Page_connexion.php
@@ -90,37 +90,37 @@
 
 <?php
 
-// Connexion à la base de données
-$connexion = pg_connect("host=localhost dbname=SkillSolidarity_Valerian user=postgres password=mfp98x");
+    // Connexion à la base de données
+    $connexion = pg_connect("host=localhost dbname=SkillSolidarity_Valerian user=postgres password=mfp98x");
 
-// Vérification de la connexion
-if (!$connexion) {
-    die("Echec de la connexion : " . pg_last_error());
-}
-
-// Vérification de la bonne soumission du formulaire
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    // Récupération des données trasmises par l'utilisateur
-    $email = pg_escape_string($connexion, $_POST["email"]);
-    $password = pg_escape_string($connexion, $_POST["password"]);
-
-    // Vérification de la validité des informations grâce aux requêtes SQL
-    $requete = "SELECT * FROM Utilisateurs WHERE email = '$email' AND mot_de_passe = '$password'";
-    $resultat = pg_query($connexion, $requete);
-
-    // Vérification du résultat de la requête
-    if (pg_num_rows($resultat) == 1) {
-        // L'utilisateur est authentifié avec succès
-        // Redirection vers une autre page
-        header("Location: autre_page.php");
-        exit(); // Assure que le script ne continue pas à s'exécuter après la redirection
-    } else {
-        // L'utilisateur n'est pas authentifié
-        echo "Les identifiants sont incorrects. Veuillez réessayer.";
+    // Vérification de la connexion
+    if (!$connexion) {
+        die("Echec de la connexion : " . pg_last_error());
     }
-}
 
-// Fermeture de la connexion à la base de données
-pg_close($connexion);
+    // Vérification de la bonne soumission du formulaire
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        // Récupération des données trasmises par l'utilisateur
+        $email = pg_escape_string($connexion, $_POST["email"]);
+        $password = pg_escape_string($connexion, $_POST["password"]);
+
+        // Vérification de la validité des informations grâce aux requêtes SQL
+        $requete = "SELECT * FROM Utilisateurs WHERE email = '$email' AND mot_de_passe = '$password'";
+        $resultat = pg_query($connexion, $requete);
+
+        // Vérification du résultat de la requête
+        if (pg_num_rows($resultat) == 1) {
+            // L'utilisateur est authentifié avec succès
+            // Redirection vers une autre page
+            header("Location: autre_page.php");
+            exit(); // Assure que le script ne continue pas à s'exécuter après la redirection
+        } else {
+            // L'utilisateur n'est pas authentifié
+            echo "Les identifiants sont incorrects. Veuillez réessayer.";
+        }
+    }
+
+    // Fermeture de la connexion à la base de données
+    pg_close($connexion);
 
 ?>
