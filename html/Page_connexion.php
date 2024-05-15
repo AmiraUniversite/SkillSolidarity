88bdd7818c38412b4c53fe942934d9bf9efab8d2
ashrafbelhiss <ashrafbelhiss@gmail.com>
2024-05-15 22:25:34 +0200

    CSS SEPARER DU HTML MODIF NIV CSS + PAGE PUBLIER

--- html/Page_connexion.php
+++ html/Page_connexion.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page de Connexion</title>
-    <link rel="stylesheet" href="Page_connexion.css">
+    <link rel="stylesheet" href="css/Page_connexion.css">
     
 </head>
 <body>
