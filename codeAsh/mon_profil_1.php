788548db5057461f9be3fd0004137b17b414cdd6
etu <amiralbc2508@gmail.com>
2024-05-18 13:06:02 +0200

    profil

--- codeAsh/mon_profil_1.php
+++ codeAsh/mon_profil_1.php
@@ -1,12 +1,11 @@
 <?php
 // Database connection settings
 $host = 'localhost';
-$dbname = 'Skillsolidarity';
+$dbname = 'Site2';
 $user = 'postgres';
-$password = '123';
+$password = 'amira';
 $port = '5432'; // default port for PostgreSQL, change if different
 $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
-
 // Function to connect to the database
 function connectDb() {
     global $connection_string;
