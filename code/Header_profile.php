65d8c4780a5348b13a1775b779aab1a14ac8635e
aymane5trz <aymane5trz@gmail.com>
2024-05-20 15:10:48 +0200

    bd

--- code/Header_profile.php
+++ code/Header_profile.php
@@ -1,3 +1,31 @@
+<?php
+// db.php
+$host = 'localhost';
+$dbname = 'nom_BD';
+$user = 'postgres';
+$password = 'MDP';
+$port = '5432'; // port par défaut pour PostgreSQL, à changer si nécessaire
+
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password}";
+
+// Fonction pour se connecter à la base de données
+function connectDb() {
+    global $connection_string;
+    $conn = pg_connect($connection_string);
+    if ($conn) {
+        return $conn;
+    } else {
+        echo "Erreur de connexion à la base de données PostgreSQL.\n";
+        return false;
+    }
+}
+
+// Fonction pour se déconnecter de la base de données
+function disconnectDb($conn) {
+    pg_close($conn);
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -26,11 +54,31 @@
 </header>
 </body>
 </html>
+
 <?php
 session_start();
+include 'db.php';
+
+// Se connecter à la base de données
+$conn = connectDb();
+
+if ($conn) {
+    // Vos opérations de déconnexion spécifiques à la base de données, si nécessaires
+    // ...
+
+    // Fermer la connexion à la base de données
+    disconnectDb($conn);
+}
+
+// Désactiver toutes les variables de session
 session_unset();
+
+// Détruire la session
 session_destroy();
+
+// Rediriger l'utilisateur vers la page d'accueil
 header("Location: Page_Acceuil/index.php");
 exit;
 ?>
+
 
