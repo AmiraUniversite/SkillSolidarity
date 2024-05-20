ebea7b836aa6aa2eb2b2a4ff56c0f772bb5c33e1
aymane5trz <aymane5trz@gmail.com>
2024-05-20 15:03:58 +0200

    bd

--- code/Header_profile.php
+++ code/Header_profile.php
@@ -5,9 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Header</title>
 <link rel="stylesheet" href="css/Header_profile.css">
-
 </head>
-
 <body>
 <header>
     <div class="logo">
@@ -20,8 +18,19 @@
     </div>
     <div class="right-links">
         <button>Profil</button>
+        <!-- Ajouter le bouton de déconnexion -->
+        <form action="logout.php" method="POST" style="display:inline;">
+            <button type="submit">Déconnexion</button>
+        </form>
     </div>
 </header>
-
 </body>
 </html>
+<?php
+session_start();
+session_unset();
+session_destroy();
+header("Location: Page_Acceuil/index.php");
+exit;
+?>
+
